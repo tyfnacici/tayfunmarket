7bb7fe30273dd62eb2384583c266c9745fcec5ea
Tayfun Açıcı <tyfnacici@gmail.com>
2023-12-10 22:04:18 +0300

    update satis grafikleri

--- admin/assets/scripts/morris-demo.js
+++ admin/assets/scripts/morris-demo.js
@@ -6,59 +6,59 @@ $(function () {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2021 Q1',
+            beypazari: 2666,
+            sirma: null,
+            uludag: 2647
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2021 Q2',
+            beypazari: 2778,
+            sirma: 2294,
+            uludag: 2441
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2021 Q3',
+            beypazari: 4912,
+            sirma: 1969,
+            uludag: 2501
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2021 Q4',
+            beypazari: 3767,
+            sirma: 3597,
+            uludag: 5689
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2022 Q1',
+            beypazari: 6810,
+            sirma: 1914,
+            uludag: 2293
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2022 Q2',
+            beypazari: 5670,
+            sirma: 4293,
+            uludag: 1881
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2022 Q3',
+            beypazari: 4820,
+            sirma: 3795,
+            uludag: 1588
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2022 Q4',
+            beypazari: 15073,
+            sirma: 5967,
+            uludag: 5175
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2023 Q1',
+            beypazari: 10687,
+            sirma: 4460,
+            uludag: 2028
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2023 Q2',
+            beypazari: 8432,
+            sirma: 5713,
+            uludag: 1791
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['beypazari', 'sirma', 'uludag'],
+        labels: ['beypazari', 'sirma', 'uludag'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
@@ -67,13 +67,13 @@ $(function () {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
+            label: "Zonguldak",
             value: 12
         }, {
-            label: "In-Store Sales",
+            label: "Ankara",
             value: 30
         }, {
-            label: "Mail-Order Sales",
+            label: "Kocaeli",
             value: 20
         }],
         resize: true
@@ -82,37 +82,29 @@ $(function () {
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
-            y: '2006',
+            y: '2019',
             a: 100,
             b: 90
         }, {
-            y: '2007',
+            y: '2020',
             a: 75,
             b: 65
         }, {
-            y: '2008',
+            y: '2021',
             a: 50,
             b: 40
         }, {
-            y: '2009',
+            y: '2022',
             a: 75,
             b: 65
         }, {
-            y: '2010',
+            y: '2023',
             a: 50,
             b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
         }],
         xkey: 'y',
         ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        labels: ['Mandalin', 'Portakal'],
         hideHover: 'auto',
         resize: true
     });
@@ -120,37 +112,33 @@ $(function () {
     Morris.Line({
         element: 'morris-line-chart',
         data: [{
-            y: '2006',
+            y: '2018',
             a: 100,
             b: 90
         }, {
-            y: '2007',
+            y: '2019',
             a: 75,
             b: 65
         }, {
-            y: '2008',
+            y: '2020',
             a: 50,
             b: 40
         }, {
-            y: '2009',
+            y: '2021',
             a: 75,
             b: 65
         }, {
-            y: '2010',
+            y: '2022',
             a: 50,
             b: 40
         }, {
-            y: '2011',
+            y: '2023',
             a: 75,
             b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
         }],
         xkey: 'y',
         ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        labels: ['Lipton İce Tea Şeftali', 'Lipton İce Tea Limon'],
         hideHover: 'auto',
         resize: true
     });
